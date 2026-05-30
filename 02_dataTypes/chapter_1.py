5cfa5d723b54e1d5956737cad22e78eda9b48a17
Abhishek Mishra <abhi@Abhisheks-MacBook-Air.local>
2026-05-30 23:31:00 +0530

    added data types and mutability

--- 02_dataTypes/chapter_1.py
+++ 02_dataTypes/chapter_1.py
@@ -1,8 +1,23 @@
-sugar_amount = 2
-print(f"Initial sugar: {sugar_amount}")
+# Variable with initial value
+sugar_amount = 30
+print(f"Initial value of Sugur: {sugar_amount}")
 
-sugar_amount = 12
-print(f"Second Initial sugar: {sugar_amount}")
+# same variable with different
+sugar_amount = 50
+print(f"Second value of Sugur: {sugar_amount}")
 
-print(f"Id of 2: {id(2)}")
-print(f"Id of 12: {id(12)}")
+# What happens, value doesn't gets changed reference of the value stored in memory changes - immutable (value not changed - variable started pointing another space), below prints actual identity where its stored
+# Example Number : Immutable
+print(f"Id of 1: {id(30)}")
+print(f"Id of 2: {id(50)}")
+
+# Mutable Example of Sets
+
+spice_mix = set()
+
+print(f"Id of spice_mix: {id(spice_mix)}")
+print(f"Initial spice_mix: {spice_mix}")
+spice_mix.add("ginger")
+spice_mix.add("cardmom")
+print(f"Id of spice_mix after change: {id(spice_mix)}")
+print(f"After addition spice_mix: {spice_mix}")
--- 02_dataTypes/chapter_2.py
+++
@@ -1,7 +0,0 @@
-spice_mix = set()
-print(f"Intial spice mix id: {id(spice_mix)}")
-
-spice_mix.add("Ginger")
-spice_mix.add("cardamom")
-
-print(f"After spice mix id: {id(spice_mix)}")
